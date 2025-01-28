d75041e2240aeb50740e4df2566c0031c9dc8e85
gcarpen2106 <gcarpen2106@gmail.com>
2025-01-28 09:24:35 +0100

    Editado archivo desempeno_empleados.py

---
+++ models/desempeno_empleados.py
@@ -0,0 +1,9 @@
+# gestion_tareas/models/desempeno_empleados.py
+
+from odoo import models, fields, api
+
+class DesempenoEmpleados(models.Model):
+
+    _name = 'desempeno.empleados'
+    _description = 'Tarea de desempeno'
+    
